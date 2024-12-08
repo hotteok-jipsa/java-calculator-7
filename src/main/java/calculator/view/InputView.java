b4c3b56a47ff449241cb070cc6f3ac1ba5c11377
unh6unh6 <ruddbs3308@naver.com>
2024-12-08 23:53:30 +0900

    fix(InputView): 오타 수정

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -3,7 +3,7 @@ package calculator.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    private static final String INPUT_GUIDE_MESSAGE = "덧셈할 문자를 입력해 주세요.";
+    private static final String INPUT_GUIDE_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
 
     public void printInputGuideMessage() {
         System.out.println(INPUT_GUIDE_MESSAGE);
