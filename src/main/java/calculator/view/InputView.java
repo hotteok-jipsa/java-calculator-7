75a722de030b9e48bbb5988c606ad26864ece70d
unh6unh6 <ruddbs3308@naver.com>
2024-12-08 21:01:17 +0900

    feat: 사용자 입력 안내 문구를 출력한다

---
+++ src/main/java/calculator/view/InputView.java
@@ -0,0 +1,10 @@
+package calculator.view;
+
+public class InputView {
+    private static final String inputGuideMessage = "덧셈할 문자를 입력해 주세요.";
+
+    public void printInputGuideMessage() {
+        System.out.println(inputGuideMessage);
+    }
+
+}
