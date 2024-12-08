052fe33813191bb88c13de82150537c42f2be895
unh6unh6 <ruddbs3308@naver.com>
2024-12-08 23:00:39 +0900

    style(InputView): 상수타입에 맞게 변수 네이밍 변경

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -3,10 +3,10 @@ package calculator.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    private static final String inputGuideMessage = "덧셈할 문자를 입력해 주세요.";
+    private static final String INPUT_GUIDE_MESSAGE = "덧셈할 문자를 입력해 주세요.";
 
     public void printInputGuideMessage() {
-        System.out.println(inputGuideMessage);
+        System.out.println(INPUT_GUIDE_MESSAGE);
     }
 
     public String getInput() {
